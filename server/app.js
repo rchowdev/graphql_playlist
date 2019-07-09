53ddaa7e70acc14b7e998f220533e80543c89d40
rchow1944 <rchow2@buffalo.edu>
2019-07-09 02:14:10 -0400

    Added root query. Added mock data for testing. Added lodash for utility functions. Added graphiql to test our root query.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
 
 //Middleware
-app.use("/graphql", graphqlHTTP({}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
 
 app.listen(4000, () => {
   console.log("Now listening for requests on port 4000");
