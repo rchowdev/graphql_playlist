b66c4e53764a49aaacfccffa383ed2ca459f27ac
rchow1944 <rchow2@buffalo.edu>
2019-07-16 02:28:28 -0400

    wrote resolve functions for root queries and graphql types

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -21,7 +21,7 @@ const BookType = new GraphQLObjectType({
     author: {
       type: AuthorType,
       resolve(parent, args) {
-        //return _.find(authors, {id: parent.authorId});
+        return Author.findById(parent.authorId);
       }
     }
   })
@@ -37,7 +37,7 @@ const AuthorType = new GraphQLObjectType({
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        //return _.filter(books, {authorId: parent.id});
+        return Book.find({authorId: parent.id});
       }
     }
   })
@@ -52,26 +52,26 @@ const RootQuery = new GraphQLObjectType({
       args: {id: {type: GraphQLID}},
       resolve(parent, args) {
         //code to get data from db/ other source
-        //return _.find(books, {id: args.id});
+        return Book.findById(args.id);
       }
     },
     author: {
       type: AuthorType,
       args: {id: {type: GraphQLID}},
       resolve(parent, args) {
-        //return _.find(authors, {id: args.id});
+        return Author.findById(args.id);
       }
     },
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        //return books;
+        return Book.find({}); //If we pass empty object it returns all the Books
       }
     },
     authors: {
       type: new GraphQLList(AuthorType),
       resolve(parent, args) {
-        //return authors;
+        return Author.find({});
       }
     }
   }
