0f8dd66ba37f59360b51eb003edb1a36277eba6a
rchow1944 <rchow2@buffalo.edu>
2019-07-16 01:50:19 -0400

    Created addBook mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -94,9 +94,26 @@ const Mutation = new GraphQLObjectType({
         });
         return author.save();
       }
+    },
+    addBook: {
+      type: BookType,
+      args: {
+        name: {type: GraphQLString},
+        genre: {type: GraphQLString},
+        authorId: {type: GraphQLID}
+      },
+      resolve(parent, args) {
+        let book = new Book({
+          name: args.name,
+          genre: args.genre,
+          authorId: args.authorId
+        });
+        return book.save();
+      }
     }
   }
 });
+
 module.exports = new GraphQLSchema({
   query: RootQuery,
   mutation: Mutation
